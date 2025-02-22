90e0924c350ed574ca740a2680206f49055052e2
Ezemenahi Benedict <ezemenahi56@gmail.com>
2025-02-22 22:22:09 +0100

    Create ReviewAndRatingController.cs

---
+++ AgricHub.Presentation/Controllers/ReviewAndRatingController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricHub.Presentation.Controllers
+{
+    [Route("api/agrichub/[controller]")]
+    [ApiController]
+    public class ReviewAndRatingController : ControllerBase
+    {
+    }
+}
